43bc4bdd2d1da2c56618cddd00015f1d55bb5487
garrbows <gbowser3@gmail.com>
2021-02-24 14:51:51 -0500

    added text and options

--- app/src/main/java/edu/temple/lab3/ImageActivity.java
+++ app/src/main/java/edu/temple/lab3/ImageActivity.java
@@ -4,12 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
 import android.os.Bundle;
+import android.util.Pair;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -28,11 +32,34 @@ public class ImageActivity extends AppCompatActivity {
 
         ImageAdapter adapter = new ImageAdapter(this, options);
 
-       ListView listView;
-       listView = findViewById(R.id.imageViewer);
+       ListView listView = findViewById(R.id.imageViewer);
        Spinner spinner = findViewById(R.id.spinner);
+       TextView text = findViewById(R.id.textView);
        listView.setAdapter(adapter);
        spinner.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //showPicture(position);
+                text.setText(((Pair)parent.getItemAtPosition(position)).first.toString());
+            }
+        });
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(ImageActivity.this, "Item Selected", Toast.LENGTH_SHORT).show();
+                text.setText("Selected "+options.get((int) id));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                Toast.makeText(ImageActivity.this, "Deselected item", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
     }
 
 }
