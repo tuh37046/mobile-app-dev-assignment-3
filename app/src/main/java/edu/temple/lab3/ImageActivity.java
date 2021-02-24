80a9d537e906670c41ccc618d151ae2cb74a4325
garrbows <gbowser3@gmail.com>
2021-02-24 14:16:50 -0500

    initial commit

---
+++ app/src/main/java/edu/temple/lab3/ImageActivity.java
@@ -0,0 +1,18 @@
+package edu.temple.lab3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+public class ImageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
